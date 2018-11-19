774033c62f33446c3379a9b6f1992c7df3bd225e
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-11-19 21:36:58 +1000

    fixed bug not making images

--- make_images.py
+++ make_images.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from PIL import Image, ImageDraw
 from pathlib import Path
+import argparse
 # from concurrent.futures import ThreadPoolExecutor
 
 
@@ -11,7 +12,7 @@ def draw_it(raw_strokes):
     image = Image.new("P", (255,255), color=255)
     image_draw = ImageDraw.Draw(image)
 
-    for stroke in raw_strokes:
+    for stroke in eval(raw_strokes):
         for i in range(len(stroke[0])-1):
 
             image_draw.line([stroke[0][i],
@@ -21,24 +22,9 @@ def draw_it(raw_strokes):
                             fill=0, width=6)
 
     # image
-    small_img = image.resize((31,31))
+    # small_img = image.resize((31,31))/
     # return small_img
-    return image.convert('RGB') # maybe just return an image?
-
-
-# def create_images(drawings, save_dir):
-#     #need to create save dir
-# 	save_dir.mkdir(parents=True, exist_ok=True)
-#     # x = 0
-#     for key, value in drawings.items():
-#         while x > 200:
-#             img = draw_it(value)
-#             name = str(key)+".jpeg"
-#             img.save(save_dir/name), "jpeg")
-#             x = x + 1
-# 		# df = pd.DataFrame(value)
-# 		# df.to_csv(save_dir/str(key))
-		# print(f'Saving Image {key}')
+    return np.array(image) # maybe just return an image?
 
 
 def create_images(drawings,save_dir):
@@ -46,7 +32,8 @@ def create_images(drawings,save_dir):
     for key, value in drawings.items():
             img = draw_it(value)
             name = str(key)+".jpeg"
-            img.save(save_dir/name, "jpeg")
+            im = Image.fromarray(img, "L")
+            im.save(save_dir/name,)
 
 
 def get_drawings(df):
@@ -54,7 +41,7 @@ def get_drawings(df):
 	# print("Creating Dict")
 	print("creating dict")
 	col = ['key_id','drawing' ]
-	df = df[col].head(n=1000)
+	df = df[col].head(n=1000) #only process 1000 images
 	df = df.set_index('key_id').to_dict()
 	return df["drawing"]
 
@@ -93,8 +80,13 @@ def main(path, save_dir):
 
 #     #need iterate through data dir and make list of classes
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_dir", type=Path, help="Splits image data sets into train and validation")
+    parser.add_argument("outdir", type=Path, help="out put directory")
+    # parser.add_argument("data_dir", type=Path, help="")
+    args = parser.parse_args()
 
-    main("/home/ubuntu/data/raw_data", "/home/ubuntu/data/images")
+    main(args.data_dir, args.outdir)
 
 
 
